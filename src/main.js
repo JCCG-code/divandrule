15346ff22b5158bb7e55d6a8f08b0a4a3272887c
Juan Carlos Cuadrado Gracia <juanky.141998@gmail.com>
2022-05-22 16:58:30 +0200

    Update baseURL

--- src/main.js
+++ src/main.js
@@ -8,8 +8,7 @@ import VueAxios from 'vue-axios'
 
 
 // Axios configuration
-axios.defaults.baseURL = 'http://localhost:5001/api'
-// axios.defaults.headers.common['Authorization'] = 'Bearer ' + localStorage.getItem('token')
+axios.defaults.baseURL = 'http://ec2-35-87-211-243.us-west-2.compute.amazonaws.com/api' || 'http://localhost:5001/api'
 
 
 // Create the frontend app by Vue.js
